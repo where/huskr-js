6561618e38a6d317aa1c96e8ac63c662ac62291f
Jean-Charles Sisk <jasisk@gmail.com>
2012-10-11 19:26:05 -0400

    WIP relevant time with pubsub

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -43,7 +43,9 @@
   window.huskr = window.huskr || {
     models: {},
     collections: {},
-    views: {}
+    views: {},
+    utils: {},
+    PubSub: _.extend({}, Backbone.Events)
   };
 
   window.huskr.models.User = Backbone.Model.extend();
@@ -52,15 +54,38 @@
       if ( ! this.get('created_at') ) {
         this.set({ 'created_at': new Date().toISOString() });
       }
+    },
+    newerThan: function( timestamp ) {
+      return Date.parse( this.get("created_at") ) < Date.parse( timestamp );
     }
   });
   window.huskr.collections.statusList = new (Backbone.Collection.extend({
     url: 'http://huskr.herokuapp.com/api/v1/statuses.json',
     model: window.huskr.models.Status,
+    initialize: function() {
+      this.on("change:created_at", function() {
+        this.sort({ silent: true });
+      }, this );
+    },
     comparator: function( model ) {
       return new Date( model.get('created_at') );
+    },
+    newerThan: function( timestamp ) {
+      return this.filter(function() {
+        return this.newerThan( timestamp );
+      });
     }
   }));
+
+  window.huskr.utils.formatDate = function( date ) {
+    return Date.create( date ).relative();
+  };
+
+  _.extend( window.huskr.PubSub, {
+    subscribe: function() {},
+    unsubscribe: function() {},
+    _seconds: function() {}
+  } );
 
   $(function(){
     window.huskr.views.Status = Backbone.View.extend({
@@ -68,25 +93,56 @@
       className: "row",
       template: _.template( $("#status-view").html() ),
       initialize: function() {
-        _.bindAll( this, "render" );
-        this.model.on( "change", this.render );
+        _.bind( "renderTime", this );
+        this.manageSubscriptions();
+        this.model.on( "change", this.render, this );
       },
       render: function() {
-        $( this.el ).html( this.template(this.model.toJSON()) );
+        var keys = _.keys(this.model.changedAttributes());
+        if ( keys.length === 1 && keys[0] === "created_at" ) {
+          this.updateTime();
+        } else {
+          this.$el.html( this.template(this.model.toJSON()) );
+        }
         return this;
+      },
+      renderTime: function() {
+        $( ".relative-time", this.$el ).text(
+          window.huskr.utils.formatDate( this.model.get("created_at") );
+        );
+      },
+      updateTime: function() {
+        this.manageSubscriptions();
+        this.renderTime();
+      },
+      manageSubscriptions: function() {
+        if ( this.model.newerThan((1).minutesBefore("now")) ) {
+          window.huskr.PubSub.unsubscribe(
+            "minute-tick", this.updateTime, this
+          );
+          window.huskr.PubSub.subscribe(
+            "second-tick", this.updateTime, this
+          );
+        } else {
+          window.huskr.PubSub.subscribe(
+            "minute-tick", this.updateTime, this
+          );
+          window.huskr.PubSub.unsubscribe(
+            "second-tick", this.updateTime, this
+          );
+        }
       }
     });
 
     window.huskr.views.StatusList = Backbone.View.extend({
       el: $("#statuses"),
       initialize: function() {
-        _.bindAll( this, "renderOne", "render" );
-        this.collection.bind( "reset", this.render );
-        this.collection.bind( "add", this.renderOne );
+        this.collection.on( "reset", this.render, this );
+        this.collection.on( "add", this.renderOne, this );
       },
       render: function() {
         this.$el.empty();
-        this.collection.each( this.renderOne );
+        this.collection.each( this.renderOne, this );
       },
       renderOne: function( status ) {
         var view = new window.huskr.views.Status({ model: status });
@@ -119,6 +175,21 @@
 
       return false;
     });
+
+    // // Set up relative-date timers
+    // if ( window.huskr.timers.minute ) {
+    //   clearInterval( window.huskr.timers.minute );
+    //   var newerModels = window.huskr.collections.statusList.newerThan(
+    //     ( 1 ).minutesBefore( "now" )
+    //   );
+    //   window.huskr.timers.minute = setInterval( function() {
+    //     //////////////
+    //     newerModels = window.huskr.collections.statusList.newerThan.call(
+    //       newerModels,
+    //       ( 1 ).minutesBefore( "now" )
+    //     );
+    //   }, 1000 );
+    // }
 
     window.huskr.collections.statusList.fetch();
 
